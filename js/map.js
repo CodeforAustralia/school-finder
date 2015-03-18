00c9e0031a62cd35e4f0c4d4411e3a0681b16df1
Peter W <techieshark@gmail.com>
2015-03-19 02:33:14 +1100

    add and use code to view nearby schools

--- js/map.js
+++ js/map.js
@@ -19,6 +19,45 @@ Map.onMouseOverOut = function (e) {
   }
 };
 
+// Fetch nearby schools and add them to the map for context
+Map.prototype.loadNearby = function () {
+  var that = this;
+
+  // get current view's bounding box
+  var bounds = this.map.getBounds();
+
+  // query for nearby schools
+  // http://postgis.refractions.net/docs/ST_MakeEnvelope.html ST_MakeEnvelope(left, bottom, right, top, [srid]) SRID defaults to 4326
+  var left = bounds.getWest();
+  var bottom = bounds.getSouth();
+  var right = bounds.getEast();
+  var top = bounds.getNorth();
+  app.sql.execute("SELECT s.* FROM " + app.db.points + " AS s " +
+      "WHERE s.the_geom && ST_MakeEnvelope(" + left + "," + bottom + ", " + right + "," + top + ") " +
+      "AND s.level_of_schooling ~* '" + app.level + "' " +
+      "AND s.school_code != " + this.row.school_code)
+    .done(function (data) {
+      // add schools (except this one, already added) to map
+      console.log(data);
+      data.rows.forEach(function (row) {
+        // Specify a Maki icon name, hex color, and size (s, m, or l).
+        // An array of icon names can be found in L.MakiMarkers.icons or at https://www.mapbox.com/maki/
+        // Lowercase letters a-z and digits 0-9 can also be used. A value of null will result in no icon.
+        // Color may also be set to null, which will result in a gray marker.
+        var icon = L.MakiMarkers.icon({icon: "school", opacity: 0.5, color: "#F5B4CC", size: "m"});
+        L.marker([row.latitude, row.longitude], {icon: icon})
+          .addTo(that.map)
+          // note we're using a bigger offset on the popup to reduce flickering;
+          // since we hide the popup on mouseout, if the popup is too close to the marker,
+          // then the popup can actually sit on top of the marker and 'steals' the mouse as the cursor
+          // moves near the edge between the marker and popup, making the popup flicker on and off.
+          .bindPopup("<b>" + row.school_name + "</b>", {offset: [0, -28]})
+          .on('mouseover', Map.onMouseOverOut)
+          .on('mouseout', Map.onMouseOverOut);
+      });
+    });
+};
+
 Map.prototype.init = function () {
 
   // initiate leaflet map
@@ -29,6 +68,10 @@ Map.prototype.init = function () {
   });
   this.map = map;
   var that = this;
+
+  map.on('viewreset moveend', function () {
+    that.loadNearby();
+  });
 
   L.tileLayer(app.geo.tiles, { attribution: app.geo.attribution }).addTo(map);
 
