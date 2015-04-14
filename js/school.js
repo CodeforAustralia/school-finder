781b96a490db81628946e6825f9d7f3cb3d1195f
Peter W <techieshark@gmail.com>
2015-04-14 22:08:23 +1000

    simplify template-related code by using _.extend
    - we let _.extend copy all fields from the database and then 
      we just add to that any custom fields we’ll want in the template.

--- js/school.js
+++ js/school.js
@@ -12,56 +12,42 @@ app = app || {};
   };
 
   app.School.toTemplateContext = function (fields, i) {
-    return {
-      resultNumber: i,
-      description: fields.description,
-      school_name: fields.school_name,
-      street: fields.street,
-      town_suburb: fields.town_suburb,
-      postcode: fields.postcode,
-      school_code: fields.school_code,
-      phone: fields.phone,
-      website: fields.website.replace("http://", ""),
-      level: function () {
-        return app.level ? app.level.capitalize() : 'School';
-      },
-      grades: fields.subtype,
-      selective_school: fields.selective_school,
-      school_specialty_type: fields.school_specialty_type,
-      preschool: fields.preschool,
-      oshc: fields.oshc, /* outside school hours care */
-      oshc_provider: fields.oshc_provider,
-      distance_education: fields.distance_education,
-      intensive_english_centre: fields.intensive_english_centre,
-      latitude: fields.latitude,
-      longitude: fields.longitude,
-      established: function () {
-        // try to return something human friendly if we can parse date.
-        var d = new Date(fields.date_1st_teacher);
-        if (d) { return d.getFullYear(); }
-        return fields.date_1st_teacher;
-      },
-      school_email: fields.school_email,
-      homeAddress: app.address,
-      homeLat: app.lat,
-      homeLng: app.lng,
-      distance: function () {
-        // We don't always have a user location
-        // (e.g. if searching for a specific school)
-        // That's OK, return nothing in that case.
-        if (!app.lat || !app.lng) { return; }
 
-        var userLatLng = new L.latLng(app.lat, app.lng);
-        var schoolLatLng = new L.latLng(fields.latitude, fields.longitude);
-        var dist = userLatLng.distanceTo(schoolLatLng);
+    var context = _.extend(fields,
+      {
+        resultNumber: i,
+        website: fields.website.replace("http://", ""),
+        level: function () {
+          return app.level ? app.level.capitalize() : 'School';
+        },
+        grades: fields.subtype,
+        established: function () {
+          // try to return something human friendly if we can parse date.
+          var d = new Date(fields.date_1st_teacher);
+          if (d) { return d.getFullYear(); }
+          return fields.date_1st_teacher;
+        },
+        homeAddress: app.address,
+        homeLat: app.lat,
+        homeLng: app.lng,
+        distance: function () {
+          // We don't always have a user location
+          // (e.g. if searching for a specific school)
+          // That's OK, return nothing in that case.
+          if (!app.lat || !app.lng) { return; }
 
-        // lookup distance along road network and insert it into page when the results come back.
-        app.calculateRouteDistance(fields.latitude, fields.longitude, '#result-' + i + ' .route-distance');
+          var userLatLng = new L.latLng(app.lat, app.lng);
+          var schoolLatLng = new L.latLng(fields.latitude, fields.longitude);
+          var dist = userLatLng.distanceTo(schoolLatLng);
 
-        return "About " + app.util.roundToOne(dist / 1000) + " km";
-      },
-      opportunity_class: fields.opportunity_class
-    };
+          // lookup distance along road network and insert it into page when the results come back.
+          app.calculateRouteDistance(fields.latitude, fields.longitude, '#result-' + i + ' .route-distance');
+
+          return "About " + app.util.roundToOne(dist / 1000) + " km";
+        },
+      });
+
+    return context;
   };
 
 }());
