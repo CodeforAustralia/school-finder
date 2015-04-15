0de1a21632bf9ee1f4b043884920356db86c2edd
Peter W <techieshark@gmail.com>
2015-04-15 18:11:20 +1000

    create initial MapView

---
+++ js/views/mapview.js
@@ -0,0 +1,23 @@
+var app, Handlebars;
+app = app || {};
+
+(function () {
+
+  var MapView = function () {
+    this.$el = $('#map-container');
+    this.template = Handlebars.compile($("#map-template").html());
+  };
+
+  app.MapView = MapView;
+
+  MapView.prototype.render = function () {
+
+    var context = {};
+    var html = this.template(context);
+
+    // clean up any previous result & re-add
+    this.$el.empty();
+    this.$el.append(html);
+  };
+
+}());
