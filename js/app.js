f5e65e36d5bf732a8b343a04a69ae002e525b3a4
Peter W <techieshark@gmail.com>
2015-04-15 18:00:03 +1000

    refactor: move school name autocompleter to utils

--- js/app.js
+++ js/app.js
@@ -40,34 +40,6 @@ app = app || {};
     });
   };
 
-  var schoolNameCompleter = function (request, response) {
-    var $input = $(this.element);
-
-    var processResults = function (data) {
-      var autocompleteResponses = [];
-      var haveResponses = false;
-      data.rows.forEach(function (row) {
-        autocompleteResponses.push({value: row.school_name});
-        haveResponses = true;
-      });
-      if (haveResponses) {
-        if (data.rows.length === 1) { // exact match found
-          $input.autocomplete("close");
-          $input.val(data.rows[0].school_name);
-        } else {  // show suggestions
-          response(autocompleteResponses);
-        }
-      } else {
-        $input.autocomplete("close");
-      }
-    };
-
-    // ask cartodb for all school names starting with the request
-    // SELECT * FROM dec_schools WHERE school_name ILIKE '%sydney%'
-    var query = "SELECT school_name FROM dec_schools WHERE school_name ILIKE '" + request.term + "%'";
-    app.sql.execute(query).done(processResults);
-  };
-
 
   var addRow = function (row, i) {
     var mapID, schoolsSQL, catchmentsSQL;
@@ -242,7 +214,7 @@ app = app || {};
       }
     });
 
-    $("#schoolname").autocomplete({minLength: 3, delay: 700, source: schoolNameCompleter });
+    $("#schoolname").autocomplete({minLength: 3, delay: 700, source: app.util.schoolNameCompleter });
 
   });
 
