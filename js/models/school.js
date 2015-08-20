d9e96e150e3ab3da5477ad7640a7a6405a5dbbac
Peter W <techieshark@gmail.com>
2015-08-20 12:13:53 +1200

    don’t show features a school doesn’t have
    relates to #128

--- js/models/school.js
+++ js/models/school.js
@@ -77,6 +77,7 @@ app = app || {};
         is_opportunity_class_possible: is_opportunity_class_possible(this.type),
         is_selective_possible: is_selective_possible(this.type),
         is_specialty_possible: is_specialty_possible(this.type),
+        is_somewhat_selective: this.selective_school !== "Not Selective",
         is_intensive_english_possible: is_intensive_english_possible(this.type),
         support_offered: this.support_ids ? _.map(this.support_ids, function (id) {
           return app.supports[id];
