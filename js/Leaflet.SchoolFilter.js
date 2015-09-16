467b8fe2bc4ec0530ca08874cf89ffbcb7847c37
reekypete <poconnor31@det.nsw.edu.au>
2015-09-17 09:29:18 +1000

    156 - Filters-need-to-be-more-comprehensive - added Intensive English Centre to filter

--- js/Leaflet.SchoolFilter.js
+++ js/Leaflet.SchoolFilter.js
@@ -37,7 +37,7 @@ var L, app;
           {label: "Boys", name: "boys", sql: "s.gender = 'boys'", matchLabel: "This is a boys school.", mismatchLabel: "Not a boys school.", matchTest: function (s) { return s.gender === 'boys'; }},
           {label: "Girls", name: "girls", sql: "s.gender = 'girls'", matchLabel: "This is a girls school.", mismatchLabel: "Not a girls school.", matchTest: function (s) { return s.gender === 'girls'; }},
           {label: "Selective option", name: "selective", sql: "s.selective_school IN ('Partially Selective', 'Fully Selective')", matchLabel: "This school offers an academically selective option.", mismatchLabel: "Not offered: academically selective option.", matchTest: function (s) { return s.selective_school === 'Partially Selective' || s.selective_school === 'Fully Selective'; }},
-          {label: "Specialty option", name: "specialty", sql: "school_specialty_type NOT IN ('Comprehensive')", matchLabel: "This school offers specialized classes", mismatchLabel: "Not offered: specialized classes.", matchTest: function (s) { return s.school_specialty_type !== 'Comprehensive'; }},
+          {label: "Intensive English option", name: "intensive_english", sql: "s.intensive_english_centre ", matchLabel: "This school has an Intensive English Centre.", mismatchLabel: "Not offered: Intensive English Centre.", matchTest: function (s) { return s.selective_school === 'Partially Selective' || s.selective_school === 'Fully Selective'; }},          {label: "Specialty option", name: "specialty", sql: "school_specialty_type NOT IN ('Comprehensive')", matchLabel: "This school offers specialized classes", mismatchLabel: "Not offered: specialized classes.", matchTest: function (s) { return s.school_specialty_type !== 'Comprehensive'; }},
           {label: "Distance Classes", name: "distance", sql: "(distance_education IN ('null') OR distance_education IS NULL)", matchLabel: "This is a distance school.", mismatchLabel: "Not a distance schoool", matchTest: function (s) { return s.distance_education !== false; }},
         ],
         options: [
@@ -146,6 +146,10 @@ var L, app;
         '     <div class="feature">' +
         '      <input type="radio" id="nearby-selective" name="feature" value="selective">' +
         '      <label for="nearby-selective">Academically selective option</label>' +
+        '     </div>' +
+        '     <div class="feature">' +
+        '      <input type="radio" id="nearby-intensive_english_centre" name="feature" value="intensive_english">' +
+        '      <label for="nearby-intensive_english">Intensive English Centre</label>' +
         '     </div>' +
         '     <div class="feature">' +
         '      <input type="radio" id="nearby-specialty" name="feature" value="specialty">' +
