7bf92656ab9ca560256fcab61c3c6b9b824fcb21
Peter W <techieshark@gmail.com>
2016-10-10 14:35:34 +1300

    better logging
    - we should be able to turn logging on/off via app.debug flag

--- js/util.js
+++ js/util.js
@@ -55,6 +55,10 @@ app.util = app.util || {};
     app.sql.execute(query).done(processResults);
   };
 
-
+  if (app.debug) {
+    app.util.log = console.log.bind(console);
+  } else {
+    app.util.log = function () {}; // don't console log in production
+  }
 
 }());
