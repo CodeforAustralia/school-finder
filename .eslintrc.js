cdc78a9b5a6cc72f3f5791415a7a47afdf6c1ff7
Peter W <techieshark@gmail.com>
2016-10-10 14:34:05 +1300

    adding js linting
    - this is so we can lint js code via eslint or an eslint plugin for sublime/atom/etc

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "globals": {
+        // Put things like jQuery, etc
+        "jQuery": true,
+        "$": true,
+        "app": true,
+        "google": true,
+        "Handlebars": true,
+        "ga": true
+    },
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-console": 0, // TODO disable for production http://eslint.org/docs/rules/no-console
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
