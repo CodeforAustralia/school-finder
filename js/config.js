04d12c180de6e2acef96a8120dc766c68d08b19d
Peter W <techieshark@gmail.com>
2016-10-29 17:12:31 -0700

    refactor: better code organization
    * switch between google and mapbox geocoders via config.js
      (app.config.geoProvider = 'mapbox' or = 'google').
    * if using google, this index.html line can be commented out to save loading the mapbox geocoder code:
           <script src="js/geo/mapbox.js"></script>
    * if using mapbox, these lines can be commented in index.html:
        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-OOmUXx24cznMjeed3M-Gmjj_oOZrqbQ&libraries=places"></script>
        <script src="js/geo/google.js"></script>

--- js/config.js
+++ js/config.js
@@ -66,6 +66,7 @@ app = app || {};
   app.config = {
     searchRadius: 105 * 1000, // maximum distance (meters) someone would be expected to travel to any school before we suggest distance schools
     nearbyLimit: 5, // when searching for schools 'nearby', this is the max number to explicitely highlight
+    geoProvider: 'mapbox' // whether to use google or mapbox for geocoder, distance matrix
   };
 
   app.config.geocoder = {
